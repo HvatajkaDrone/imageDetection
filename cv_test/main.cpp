384ef64a2ea248a60fdb9c4d0ae591846273473f
Andrey Skuratov <skurat95@yandex.ru>
2016-01-30 13:46:16 +0300

    start project

---
+++ cv_test/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>;
+#include "opencv2\core\core.hpp";
+#include "opencv2\highgui\highgui.hpp";
+#include "opencv2\imgproc\imgproc.hpp";
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat image;
+	image = imread("deva.jpg", CV_LOAD_IMAGE_COLOR);
+
+	if (!image.data)
+	{
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+
+	// Create a new matrix to hold the gray image
+	Mat gray;
+
+	// convert RGB image to gray
+	cvtColor(image, gray, CV_BGR2GRAY);
+
+	namedWindow("Display window", CV_WINDOW_AUTOSIZE);
+	imshow("Display window", image);
+
+	namedWindow("Result window", CV_WINDOW_AUTOSIZE);
+	imshow("Result window", gray);
+
+	waitKey(0);
+	return 0;
+}
