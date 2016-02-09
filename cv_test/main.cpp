b1770a53a6d07ae1700102aa80a2014178110cdf
Andrey Skuratov <skurat95@yandex.ru>
2016-02-09 19:11:38 +0300

    Capture frame

--- cv_test/main.cpp
+++ cv_test/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>;
 #include "opencv2\core\core.hpp";
 #include "opencv2\highgui\highgui.hpp";
+#include <stdlib.h>;
+#include <stdio.h>;
 #include "opencv2\imgproc\imgproc.hpp";
 
 using namespace std;
@@ -8,27 +10,31 @@ using namespace cv;
 
 int main()
 {
-	Mat image;
-	image = imread("deva.jpg", CV_LOAD_IMAGE_COLOR);
+	// Initialize captured frame from random camera (we have only one camera)
+	CvCapture* capture = cvCreateCameraCapture(CV_CAP_ANY);
+	assert(capture);
 
-	if (!image.data)
+	// Get width and height of capture image
+	double width = cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH),
+		height = cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
+
+	// Captured frame
+	IplImage* frame = 0;
+
+	// Create window
+	cvNamedWindow("Captured frame", CV_WINDOW_AUTOSIZE);
+
+	while (true)
 	{
-		cout << "Could not open or find the image" << std::endl;
-		return -1;
+		// Take frame from capture
+		frame = cvQueryFrame(capture);
+
+		// Show frame
+		cvShowImage("Captured frame", frame);
 	}
 
-	// Create a new matrix to hold the gray image
-	Mat gray;
+	cvReleaseCapture(&capture);
+	cvDestroyWindow("Captured frame");
 
-	// convert RGB image to gray
-	cvtColor(image, gray, CV_BGR2GRAY);
-
-	namedWindow("Display window", CV_WINDOW_AUTOSIZE);
-	imshow("Display window", image);
-
-	namedWindow("Result window", CV_WINDOW_AUTOSIZE);
-	imshow("Result window", gray);
-
-	waitKey(0);
 	return 0;
 }
